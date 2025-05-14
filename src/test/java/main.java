d67fa2bf40d465cfe20f17610924e54b1c32cbc6
rodbag206 <rbaggs0811@gamil.com>
2025-05-14 13:54:30 -0500

    Thirdcommit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -2,9 +2,7 @@ public class main {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        System.out.println("This is the project version one");
-        System.out.println("Hellow worls");
-        System.out.println("Hello world");
-        System.out.println("hey");
+        System.out.println("This is the project version 3");
+
     }
 }
