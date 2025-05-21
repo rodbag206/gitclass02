69d497b7ed46102de9ad3a0aae6ca4a5e253d4b9
rodbag206 <rbaggs0811@gamil.com>
2025-05-21 14:34:35 -0500

    person2commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
 
 
-        System.out.println("This is the project version 5");
+        System.out.println("This is the project version 6 made by person 2");
 
     }
 }
